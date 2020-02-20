f4c1e55376a2ad8a44d6d0ef94532b90159daf60
enriavil1 <eavilaasapche@gmail.com>
2020-02-20 12:53:12 +0100

    python webscrapping using webdriver to get into github and create a new repo

---
+++ create.py
@@ -0,0 +1,97 @@
+import os
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+path= "/Users/enriavil1/Desktop/project/Python/"
+try:
+    browser= webdriver.Safari()
+
+except Exception:
+    browser= webdriver.Chrome()
+
+browser.get("https://github.com/login")
+enter_key= u'\ue007'
+
+def create():
+
+    name = str(sys.argv[1])
+    username= str(os.environ["username"])
+    password= str(os.environ["password"])
+
+    os.mkdir(path + name)
+
+    #putting your username in
+    button= browser.find_elements_by_xpath("//*[@id='login_field']")[0]
+
+    try:
+        button.send_keys(username)
+    
+    except Exception:
+        print("no username inputed")
+
+    #putting your password in
+    button= browser.find_elements_by_xpath("//*[@id='password']")[0]
+
+    try:
+        button.send_keys(password)
+    
+    except Exception:
+        print("no password inputed")
+    
+    #login in
+    button= browser.find_elements_by_xpath("//input[@name= 'commit']")[0]
+    button.send_keys(enter_key)
+
+    time.sleep(3)
+    browser.get("https://github.com/new")
+
+
+    #putting new repo name
+    button= WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.XPATH, "//*[@id='repository_name']")))
+    button.send_keys(name)
+
+    #pressing creating repo"//*[@id='new_repository']/div[3]/button"
+    time.sleep(2)
+    button= browser.find_element_by_xpath("//*[@id='new_repository']/div[3]/button")
+    button.send_keys(enter_key)
+
+    #getting the remote
+    time.sleep(3)
+    remote_box= browser.find_elements_by_xpath("//*[@id='empty-setup-clone-url']")[0]
+    remote_url= remote_box.get_attribute("value")
+
+    browser.close()
+
+    
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    create()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
